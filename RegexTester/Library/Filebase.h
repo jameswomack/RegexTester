3c494bbaa1e59ffd876aace7a0fe43482bb9b3ad
James Womack <jamesw@amcoonline.net>
2013-02-07 21:07:17 -0800

    gitignore

---
+++ RegexTester/Library/Filebase.h
@@ -0,0 +1,14 @@
+//
+//  Filebase.h
+//  RegexTester
+//
+//  Created by James Womack on 2/7/13.
+//
+//
+
+#ifndef RegexTester_Filebase_h
+#define RegexTester_Filebase_h
+
+
+
+#endif
---
+++ RegexTester/Library/Filebase.m
@@ -0,0 +1,14 @@
+//
+//  Filebase.m
+//  RegexTester
+//
+//  Created by James Womack on 2/7/13.
+//
+//
+
+#ifndef RegexTester_Filebase_m
+#define RegexTester_Filebase_m
+
+
+
+#endif
